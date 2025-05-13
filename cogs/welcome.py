e03c4d1787ffc6f4dbf0d5b85bca7b5051e6b189
pineda.er <epineda@csyoutsourcing.com>
2025-05-13 15:28:03 +0800

    change user free coins from 0 to 6

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -37,7 +37,7 @@ class welcome(commands.Cog):
         
         if not items:
             db_currency = db_server.collection("currency").document(str(member.id))
-            db_currency.set({"mention": member.mention,"userID": member.id, "balance": int(0)}, merge=True)
+            db_currency.set({"mention": member.mention,"userID": member.id, "balance": int(6)}, merge=True)
         
         my_image = Image.open("./image/welcome.png")
         
